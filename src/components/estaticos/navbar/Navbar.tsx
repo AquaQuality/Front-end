b6b746604c8a26c929960f271a30a8951eb5a1e4
IagoVieiraa <iago.vieiras13@gmail.com>
2022-02-18 11:14:48 -0300

    Adicionando Navbar

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense" style={{ backgroundColor: "#1E90FF" }}>
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                           AcquaQuality
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                               Inicio
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Sobre nós
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Contato
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    logout
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar;
