68029bca7a85c2bb4179eef0a8dc4ee8ddc12e64
IagoVieiraa <iago.vieiras13@gmail.com>
2022-02-23 15:16:23 -0300

    iago]

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -5,46 +5,46 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { Link, useHistory } from 'react-router-dom';
 import useLocalStorage from "react-use-localstorage";
-import { login } from "../../services/Service";
+import { login } from "../../services/Services";
 import UserLogin from "../../models/UserLogin";
 import './Login.css';
 
-function Login(){
-    let history = useHistory();
-    const [token, setToken] = useLocalStorage('token')
-    const [userLogin, setUserLogin] = useState<UserLogin>(
-        
-        {
-            id: 0,
-            usuario: '',
-            senha: '',
-            token: ''
-        })
+function Login() {
+  let history = useHistory();
+  const [token, setToken] = useLocalStorage('token')
+  const [userLogin, setUserLogin] = useState<UserLogin>(
 
-    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
-        setUserLogin({
-            ...userLogin,
-            [e.target.name]: e.target.value
-        })
+    {
+      id: 0,
+      usuario: '',
+      senha: '',
+      token: ''
+    })
+
+  function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+    setUserLogin({
+      ...userLogin,
+      [e.target.name]: e.target.value
+    })
+  }
+  useEffect(() => {
+    if (token != '') {
+      history.push('/home')
     }
-    useEffect(() => {
-        if (token != '') {
-            history.push('/home')
-        }
-    }, [token])
+  }, [token])
 
-    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault();
-        try { 
-            await login(`/usuarios/logar`, userLogin, setToken)
-            alert("Usuário logado com sucesso !")
-        } catch (error) {
-            alert("Dados do usuário inconsistentes. Erro ao logar !")
-        }
+  async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      await login(`/usuarios/logar`, userLogin, setToken)
+      alert("Usuário logado com sucesso !")
+    } catch (error) {
+      alert("Dados do usuário inconsistentes. Erro ao logar !")
     }
+  }
 
-    return(
-        <Grid container component="main" className='root'>
+  return (
+    <Grid container component="main" className='root'>
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className='imagem' />
       <Grid item xs={12} >
@@ -59,17 +59,17 @@ function Login(){
             <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
 
             <TextField variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
-            
+
             <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
             <Button type="submit" fullWidth variant="contained" color="primary" className='submit'>Sign In</Button>
-            
+
             <Box mt={5}>
             </Box>
           </form>
         </div>
       </Grid>
     </Grid>
-    );
+  );
 }
 
 export default Login;
