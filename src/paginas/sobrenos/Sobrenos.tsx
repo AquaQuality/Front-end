bc39121dc958c7bad69b011abcfe504bbb2157d8
Vitória Santos Oliveira <contato.santos.vitoria12@gmail.com>
2022-02-23 11:31:02 -0300

    alteração sobre nos vitoria

--- src/paginas/sobrenos/Sobrenos.tsx
+++ src/paginas/sobrenos/Sobrenos.tsx
@@ -81,13 +81,13 @@ function Sobrenos() {
 
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid item xs={6} style={{ marginTop: 30, paddingLeft: 130 }}>
-                    <img className='img' src="https://p2.trrsf.com/image/fget/cf/1200/1200/filters:quality(85)/images.terra.com/2022/01/15/neymar-comecou-a-sua-carreira-jogando-pelo-santos-ricardo-saibun-santos-fc.jpeg" alt="" width="300px" height="350px" />
+                    <img className='img' src="https://imgur.com/OhuIOWe" alt="" width="300px" height="350px" />
                 </Grid>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={10}>
-                        <h1>Vit fã da cici</h1>
+                        <h1>Vitoria Oliveira</h1>
                         <p>
-                            vit
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sapiente non nihil ipsam facilis assumenda eligendi saepe blanditiis a quis minus laboriosam odit soluta dolore adipisci nam, cupiditate delectus deserunt. Lorem, ipsum dolor sit amet consectetur adipisicing elit. A ab nesciunt soluta ea alias necessitatibus, quia eum voluptatibus, libero nobis deserunt ad modi. Voluptatem deserunt doloribus odit repellendus temporibus. Debitis!
                         </p>
                     </Box>
                 </Grid>
