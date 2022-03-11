5d350bff0ac8dfb8bd7f70da1f76f572df1050e0
Amanda Marques <andaemc@gmail.com>
2022-03-11 09:24:24 -0300

    Sobre nós

--- src/paginas/sobrenos/Sobrenos.tsx
+++ src/paginas/sobrenos/Sobrenos.tsx
@@ -40,7 +40,8 @@ function Sobrenos() {
                         <Grid item xs={4} className='alinhar'>
                             <div id="passar_mouse">
                                 <img src="https://i.imgur.com/cZ4j2f7.png"  alt='Amanda Marques'width="150" height="150" className='img3' />
-                                <div id="mostrar">Amanda Marques<p></p></div></div>
+                                <div id="mostrar"><b>Amanda Marques</b>
+                                <p>Sou uma mulher de família, forte, otimista e leal, não me deixo abater com as dificuldades da vida, luto pelos meus objetivos com muita garra e determinação aprendo com meus desafios e me reinvento  a cada aprendizado. Estou sempre em busca de conhecimento que me faça crescer profissionalmente e como ser humano.</p></div></div>
                             <a href="https://github.com/AmandaMFurtado" target="_blank" rel="noreferrer" >
                                 <GitHubIcon className='redes1' />
                             </a>
@@ -54,75 +55,78 @@ function Sobrenos() {
                         <Grid item xs={4} className='alinhar'>
                             <div id="passar_mouse">
                                 <img src="https://i.imgur.com/KS5OGHd.jpg"alt='Bruno Felix' width="150" height="150" className='img3' />
-                                <div id="mostrar">Bruno <p>Um fanático por tecnologia e suas diretrizes em desenvolvimento, busco sempre estar atento a tendências tecnológicas e conhecer sobre os assuntos dos quais quero me especializar e agregar
+                                <div id="mostrar"><b>Bruno Felix</b> <p>Um fanático por tecnologia e suas diretrizes em desenvolvimento, busco sempre estar atento a tendências tecnológicas e conhecer sobre os assuntos dos quais quero me especializar e agregar
                                     habilidades em minha carreira, tais como desenvolvimento de sistemas, cloud computing, banco de
                                     dados, gestão ágil entre outras.
                                 </p></div>
                             </div>
-                            <a href="https://github.com/AquaQuality" target="_blank" rel="noreferrer" >
+                            <a href="https://github.com/Brunofelix2021" target="_blank" rel="noreferrer" >
                                 <GitHubIcon className='redes1' />
                             </a>
-                            <a href="acquaquality.pi@gmail.com" target="_blank">
+                            <a href="bruno.felix.sousa@hotmail.com" target="_blank">
                                 <EmailIcon className='redes1' />
                             </a>
-                            <a href="https://www.linkedin.com/in/vit%C3%B3ria-santos-oliveira-53755b189/" target="_blank" rel="noreferrer">
+                            <a href="https://www.linkedin.com/in/bruno-felix-souza-1b249b14b/" target="_blank" rel="noreferrer">
                                 <LinkedInIcon className='redes1' />
                             </a>
                         </Grid>
                         <Grid item xs={4} className='alinhar'>
                             <div id="passar_mouse">
                                 <img src="https://i.imgur.com/5dVrzyQ.jpg" alt='Cibele França' width="150" height="150" className='img3' />
-                                <div id="mostrar">Cibele França<p>
+                                <div id="mostrar"><b>Cibele França</b><p>
                                     Sou uma pessoa bastante comunicativa e extrovertida, por isso, gosto de fazer o lugar no qual estou um lugar engraçado e descontraído. Gosto de sair com meus amigos, assistir séries e ouvir músicas.
 
                                     Procuro sempre estar aprendendo sobre tudo, novas culturas, novas histórias. Gosto muito de me comunicar com tudo, sou uma pessoa muito esforçada, se tem algo que eu não sei eu sempre faço de tudo para entender e aprender, e por meio da tecnologia pretendo ajudar o máximo de pessoas que eu conseguir.
                                 </p></div></div>
-                            <a href="https://github.com/AquaQuality" target="_blank" rel="noreferrer" >
+                            <a href="https://github.com/cibelefranca" target="_blank" rel="noreferrer" >
                                 <GitHubIcon className='redes1' />
                             </a>
-                            <a href="acquaquality.pi@gmail.com" target="_blank">
+                            <a href="cibeletessinha@gmail.com" target="_blank">
                                 <EmailIcon className='redes1' />
                             </a>
-                            <a href="https://www.linkedin.com/in/vit%C3%B3ria-santos-oliveira-53755b189/" target="_blank" rel="noreferrer">
+                            <a href="https://www.linkedin.com/in/cibele-fran%C3%A7a-55157520a/" target="_blank" rel="noreferrer">
                                 <LinkedInIcon className='redes1' />
                             </a>
                         </Grid>
                         <Grid item xs={3} className='alinhar'>
                             <div id="passar_mouse">
                                 <img src="https://imgur.com/3zTAXAL.jpg" alt='Iago Vieira' width="150" height="150" className='img3' />
-                                <div id="mostrar">Iago Vieira<p>Tenho 21 anos e sou um apaixonado por tecnologia. Procuro sempre me mantar informado nas novas tecnologias do mercado e me aperfeiçoar mais. Espero contribuir para um mundo cada vez mais integrado e conectado</p></div></div>
-                            <a href="https://github.com/AquaQuality" target="_blank" rel="noreferrer" >
+                                <div id="mostrar"><b>Iago Vieira</b>
+                                <p>Tenho 21 anos e sou um apaixonado por tecnologia. Procuro sempre me mantar informado nas novas tecnologias do mercado e me aperfeiçoar mais. Espero contribuir para um mundo cada vez mais integrado e conectado</p></div></div>
+                            <a href="https://github.com/IagoVieiraa" target="_blank" rel="noreferrer" >
                                 <GitHubIcon className='redes1' />
                             </a>
-                            <a href="acquaquality.pi@gmail.com" target="_blank">
+                            <a href="iago.vieiras13@gmail.com" target="_blank">
                                 <EmailIcon className='redes1' />
                             </a>
-                            <a href="https://www.linkedin.com/in/vit%C3%B3ria-santos-oliveira-53755b189/" target="_blank" rel="noreferrer">
+                            <a href="https://www.linkedin.com/in/iago-vieira-499076165/" target="_blank" rel="noreferrer">
                                 <LinkedInIcon className='redes1' />
                             </a>
                         </Grid>
                         <Grid item xs={3} className='alinhar'>
                             <div id="passar_mouse">
                                 <img src="https://i.imgur.com/lVwgmAG.jpg" alt='Lucas Oi' width="150" height="150" className='img3' />
-                                <div id="mostrar">Lucas Oi<p> Formado como pessoa desenvolvedora Fullstack Java JR. pela Generation Brasil, além de cursar o 6º ciclo de engenharia elétrica, estou sempre em busca de novos desafios, gosto de café, estar na natureza e tocar violão.</p></div></div>
-                            <a href="https://github.com/AquaQuality" target="_blank" rel="noreferrer" >
+                                <div id="mostrar"><b>Lucas Oi</b>
+                                <p> Formado como pessoa desenvolvedora Fullstack Java JR. pela Generation Brasil, além de cursar o 6º ciclo de engenharia elétrica, estou sempre em busca de novos desafios, gosto de café, estar na natureza e tocar violão.</p></div></div>
+                            <a href="https://github.com/LucasOii" target="_blank" rel="noreferrer" >
                                 <GitHubIcon className='redes1' />
                             </a>
-                            <a href="acquaquality.pi@gmail.com" target="_blank">
+                            <a href="lucas.oi-@hotmail.com" target="_blank">
                                 <EmailIcon className='redes1' />
                             </a>
-                            <a href="https://www.linkedin.com/in/vit%C3%B3ria-santos-oliveira-53755b189/" target="_blank" rel="noreferrer">
+                            <a href="https://www.linkedin.com/in/lucas-de-oliveira-oi/" target="_blank" rel="noreferrer">
                                 <LinkedInIcon className='redes1' />
                             </a>
                         </Grid>
                         <Grid item xs={3} className='alinhar'>
                             <div id="passar_mouse">
                                 <img src="https://i.imgur.com/OhuIOWe.jpg" alt='Vitoria Oliveira' width="150" height="150" className='img3' />
-                                <div id="mostrar">Vitoria Oliveira</div></div>
-                            <a href="https://github.com/AquaQuality" target="_blank" rel="noreferrer" >
+                                <div id="mostrar"><b>Vitoria Oliveira</b><p>Desenvolvedora Full Stack Java Jr, estudante de Engenharia da Computação. Busco sempre novos conhecimentos, Tenho facilidade em me adaptar a novas rotinas, gosto de resolver problemas e gosto de trabalhar em equipe.
+</p></div></div>
+                            <a href="https://github.com/viisoli" target="_blank" rel="noreferrer" >
                                 <GitHubIcon className='redes1' />
                             </a>
-                            <a href="acquaquality.pi@gmail.com" target="_blank">
+                            <a href="contato.santos.vitoria12@gmail.com" target="_blank">
                                 <EmailIcon className='redes1' />
                             </a>
                             <a href="https://www.linkedin.com/in/vit%C3%B3ria-santos-oliveira-53755b189/" target="_blank" rel="noreferrer">
@@ -132,7 +136,7 @@ function Sobrenos() {
                         <Grid item xs={3} className='alinhar'>
                             <div id="passar_mouse">
                                 <img src="https://i.imgur.com/yS7HM9o.jpg" alt='Vitoria Romão' width="150" height="150" className='img3' />
-                                <div id="mostrar">Vitoria Romão<p>Adoro desafios que aumentam meu horizonte e mudam meu modo de ver a vida, por esse motivo decidi fazer a transição de carreira. Sou uma pessoa criativa, que ama novos conhecimentos, e com isso me encontrei como desenvolvedora. Acredito que levarei boa parte da minha bagagem adquirida nessa nova etapa da minha jornada.</p></div></div>
+                                <div id="mostrar"><b>Vitoria Romão</b><p>Adoro desafios que aumentam meu horizonte e mudam meu modo de ver a vida, por esse motivo decidi fazer a transição de carreira. Sou uma pessoa criativa, que ama novos conhecimentos, e com isso me encontrei como desenvolvedora. Acredito que levarei boa parte da minha bagagem adquirida nessa nova etapa da minha jornada.</p></div></div>
                             <a href="https://github.com/vitoriaromao" target="_blank" rel="noreferrer" >
                                 <GitHubIcon className='redes1' />
                             </a>
